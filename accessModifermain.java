9143251581a061db0643c6f436993b87d44430f0
srija2526 <srijasaisrija908@gmail.com>
2025-11-07 08:32:05 +0530

    Add files via upload

---
+++ accessModifermain.java
@@ -0,0 +1,21 @@
+package com.avn.tns.modifer;
+
+public class accessModifermain {
+    public static void main(String[] args) {
+        accessModifer a=new accessModifer();
+        System.out.println(a.collegename);
+        System.out.println(a.branch);
+        //System.out.println(a.id);public only within class
+        System.out.println(a.name);
+    }
+    
+    
+
+
+    
+
+    
+
+
+
+}
