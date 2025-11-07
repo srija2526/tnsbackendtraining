c7bb317cadd610709fd9ef6d061ea499452fd887
srija2526 <srijasaisrija908@gmail.com>
2025-11-07 08:31:36 +0530

    Add files via upload

---
+++ accessModifer.java
@@ -0,0 +1,16 @@
+package com.avn.tns.modifer;
+
+public class accessModifer {
+    private int  id=50;
+    public String collegename="avn";
+    protected String name="saisrija";
+    //default
+    String branch="cse";
+    void college(){
+        System.out.println("hello");
+        
+    }
+      
+    
+
+}
