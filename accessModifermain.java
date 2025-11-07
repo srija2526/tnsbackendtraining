211e066c5f1a4dae133f63fa279533e66636236d
srija2526 <srijasaisrija908@gmail.com>
2025-11-07 08:35:20 +0530

    Add files via upload

--- accessModifermain.java
+++ accessModifermain.java
@@ -3,10 +3,10 @@ package com.avn.tns.modifer;
 public class accessModifermain {
     public static void main(String[] args) {
         accessModifer a=new accessModifer();
-        System.out.println(a.collegename);
-        System.out.println(a.branch);
-        //System.out.println(a.id);public only within class
-        System.out.println(a.name);
+        System.out.println(a.collegename);//public
+        System.out.println(a.branch);//default only within package
+        //System.out.println(a.id);private only within class
+        System.out.println(a.name);//protected only within package and outside package subclass thats extends this class
     }
     
     
