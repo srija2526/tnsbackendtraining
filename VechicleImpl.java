43ca8fb5412a52cd59f569cdd71f51c045d7d409
srija2526 <srijasaisrija908@gmail.com>
2025-11-11 20:03:06 +0530

    Add files via upload

---
+++ VechicleImpl.java
@@ -0,0 +1,11 @@
+package com.avn.tns.oopsdemo;
+
+public class VechicleImpl extends Vechicle{
+    public void start(){
+        System.out.println("vehicle engine starts");
+    }
+    public void stop(){
+        System.out.println("vehicle engine stops");
+    }
+
+}
