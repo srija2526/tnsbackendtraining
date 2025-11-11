52889f7c92367e2920009e9638f5c3ccb37bada9
srija2526 <srijasaisrija908@gmail.com>
2025-11-11 20:02:49 +0530

    Add files via upload

---
+++ Vechicle.java
@@ -0,0 +1,14 @@
+package com.avn.tns.oopsdemo;
+
+public abstract class Vechicle {
+
+    abstract void start();
+    abstract void stop();
+
+    void FuelTye(){
+        System.out.println("petrol");
+    }
+
+    
+
+}
