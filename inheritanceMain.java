1d071028a72c687d68761a226ca2d5346da9b8f8
srija2526 <srijasaisrija908@gmail.com>
2025-11-11 07:53:43 +0530

    Add files via upload

---
+++ inheritanceMain.java
@@ -0,0 +1,9 @@
+package com.avn.tns.oopsdemo;
+
+public class inheritanceMain {
+    public static void main(String[] args) {
+        ElectriCar e=new ElectriCar("honda", "ab", 100);
+        e.startEngine();
+    }
+
+}
