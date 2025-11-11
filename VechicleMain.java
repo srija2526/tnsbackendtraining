b2e90399c6a9eba3cd15d7acbf2963fdf5a799f7
srija2526 <srijasaisrija908@gmail.com>
2025-11-11 20:03:20 +0530

    Add files via upload

---
+++ VechicleMain.java
@@ -0,0 +1,10 @@
+package com.avn.tns.oopsdemo;
+
+public class VechicleMain {
+    public static void main(String[] args) {
+        Vechicle v=new VechicleImpl();
+        v.start();
+        v.stop();
+    }
+
+}
