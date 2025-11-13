5cf3392294456cf54b950417492f61329da553e7
srija2526 <srijasaisrija908@gmail.com>
2025-11-13 17:21:08 +0530

    Add files via upload

---
+++ CustomClass.java
@@ -0,0 +1,11 @@
+package com.avn.tns.oopsdemo;
+
+public class CustomClass {
+    String name;
+    int no;
+    void CustomClass(String name,int no){
+        this.name=name;
+        this.no=no;
+    }
+
+}
---
+++ CustonClassMain.java
@@ -0,0 +1,10 @@
+package com.avn.tns.oopsdemo;
+
+public class CustonClassMain {
+    public static void main(String[] args) {
+         record person(String name,int no){};
+
+         person[] p={new person("kavya",23),new person("divya",24)};
+    }
+
+}
